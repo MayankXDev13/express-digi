39d42de15f470f26acf7f51bbb10ff317bdffba0
MayankXDev13 <vijaymaynk2003@gmail.com>
2025-03-28 15:28:19 +0530

    Add home route to respond with a greeting message

--- 02_express/index.js
+++ 02_express/index.js
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
+app.get('/', (req, res) => {
+    res.send('Hello from Mayank and his tea!')
+})
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
