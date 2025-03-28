01e92c3bba97576a07321003bec58807fc3d159e
MayankXDev13 <vijaymaynk2003@gmail.com>
2025-03-28 16:29:01 +0530

    Add PUT and DELETE routes for updating and deleting teas

--- 02_express/index.js
+++ 02_express/index.js
@@ -28,13 +28,36 @@ app.get("/teas", (req, res) => {
 });
 
 // get  a tea with id
-
 app.get("/teas/:id", (req, res) => {
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
   if (!tea) {
     return res.status(404).send(`Tea not found`);
   }
   res.status(200).send(tea);
+});
+
+// update tea
+
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send(`Tea not found`);
+  }
+  const { name, price } = req.body;
+  (tea.name = name), (tea.price = price);
+  res.status(200).send(tea);
+});
+
+// delete tea
+
+app.delete("teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    return res.status(404).send("tea not found");
+  }
+  teaData.splice(index, 1);
+  return res.status(204).send("deleted");
 });
 
 app.listen(port, () => {
