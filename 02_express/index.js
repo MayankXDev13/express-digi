f187df664276afbb9d698a47ed4b5a42c4bdcea6
MayankXDev13 <vijaymaynk2003@gmail.com>
2025-03-28 16:09:14 +0530

    Add GET route to retrieve a tea by ID and fix response status code for all teas

--- 02_express/index.js
+++ 02_express/index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 let teaData = [];
 let nextId = 1;
 
+// add a new tea
+
 app.post("/teas", (req, res) => {
   const { name, price } = req.body;
   const newTea = {
@@ -20,8 +22,19 @@ app.post("/teas", (req, res) => {
   res.status(201).send(newTea);
 });
 
+// get all tea
 app.get("/teas", (req, res) => {
-  res.statusCode(200).send(teaData);
+  res.status(200).send(teaData);
+});
+
+// get  a tea with id
+
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send(`Tea not found`);
+  }
+  res.status(200).send(tea);
 });
 
 app.listen(port, () => {
