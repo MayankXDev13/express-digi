82888ba14576c38f48f81caec5699e7dfc4079f2
MayankXDev13 <vijaymaynk2003@gmail.com>
2025-03-28 15:59:26 +0530

    Add POST route for teas to create new tea entries

--- 02_express/index.js
+++ 02_express/index.js
@@ -3,20 +3,24 @@ import express from "express";
 const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
+app.use(express.json());
+
+let teaData = [];
+let nextId = 1;
+
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = {
+    id: nextId++,
+    name,
+    price,
+  };
+
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
 
 
-app.get('/', (req, res) => {
-    res.send('Hello from Mayank and his tea!')
-})
-
-app.get('/ice-tea', (req, res) => {
-    res.send('what ice tea would you prefer?')
-})
-
-
-app.get('/twitter', (req, res) => {
-    res.send('MayankxDev.com')
-})
 
 app.listen(port, () => {
   console.log(`Server is listeng at http://${hostname}:${port}`);
