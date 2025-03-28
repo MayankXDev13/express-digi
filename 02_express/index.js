6a16a23f9e9ef3d62fe6453fa45ce8c8c843824a
MayankXDev13 <vijaymaynk2003@gmail.com>
2025-03-28 15:30:42 +0530

    Update server log message to include hostname in Express app

--- 02_express/index.js
+++ 02_express/index.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const hostname = "127.0.0.1";
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Mayank and his tea!')
 })
 
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+  console.log(`Server is listeng at http://${hostname}:${port}`);
 });
